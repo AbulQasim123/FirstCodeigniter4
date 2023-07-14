7eb66b89df8d0191b4f4b8c31ec861213572dea4
qasimabul <ansariabulqasim48@gmail.com>
2023-07-14 22:56:17 +0530

    make navigation and Content area

--- app/Views/users/dashboard.php
+++ app/Views/users/dashboard.php
@@ -1,112 +1,91 @@
 <?= $this->extend('layouts/master') ?>
 <?= $this->section('content') ?>
 
-<style>
-    /* Custom CSS for the navigation panel */
-    .navigation-panel {
-        background-color: #f1f1f1;
-        /* height: calc(100vh - 64px); */
-        height: 100vh;
-        /* Adjust the height based on your header height */
-        overflow-y: auto;
-        padding: 10px;
-    }
-
-    .navigation-panel ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    }
-
-    .navigation-panel li {
-        margin-bottom: 4px;
-    }
-
-    .navigation-panel a {
-        display: block;
-        padding: 8px;
-        text-decoration: none;
-        color: #333;
-        transition: background-color 0.3s ease;
-    }
-
-    .navigation-panel a:hover {
-        background-color: #ddd;
-    }
-
-    .row .col {
-        padding: 0 0rem;
-    }
-
-    nav.navbar-fixed {
-        position: fixed;
-        top: 0;
-        width: 100%;
-        z-index: 9999;
-    }
-</style>
 <!-- Header -->
-<header class="blue lighten-1">
-    <nav class="navbar-fixed">
-        <div class="nav-wrapper">
-            <a href="#!" class="brand-logo">Logo</a>
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">Home</a></li>
-                <li><a href="badges.html">About</a></li>
-                <li><a href="collapsible.html">Contact Us</a></li>
-                <li><a href="mobile.html">More</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<nav class="red lighten-3">
+    <div class="">
+        <a href="#!" class="brand-logo">
+            <img src="<?= base_url('assets/images/ci.png') ?>" alt="">
+        </a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a class="black-text" href="">Home</a></li>
+            <li><a class="black-text" href="">About</a></li>
+            <li><a class="black-text" href="">Contact Us</a></li>
+            <li><a class="black-text" href="<?= base_url('profile') ?>">Profile</a></li>
+            <li><a class="black-text" href="<?= base_url('logout') ?>">Logout</a></li>
+        </ul>
+    </div>
+</nav>
+
 
 <!-- Mobile Navigation -->
 <ul class="sidenav" id="mobile-demo">
     <li><a href="#" class="right"><i class="material-icons">close</i></a></li>
-    <li><a href="sass.html">Home</a></li>
-    <li><a href="badges.html">About</a></li>
-    <li><a href="collapsible.html">Contact Us</a></li>
-    <li><a href="mobile.html">More</a></li>
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Contact Us</a></li>
+    <li><a href=<?= site_url('profile') ?>">More</a></li>
+    <li><a href="<?= site_url('logout') ?>">Logout</a></li>
 </ul>
 
 <div class="row">
-    <div class="col s3">
-        <!-- Grey navigation panel -->
-        <div class="navigation-panel">
+    <div class="col s2">
+        <div id="navbar">
             <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
+                <li>Hello, <?= session()->get('name') ?></li>
+            </ul>
+            <ul>
+                <li><a href="#Use-css" class="nav-links">Use Css</a></li>
+                <li><a href="#Module" class="nav-links">Module</a></li>
+                <li><a href="#solvingcommoncss-problem" class="nav-links">Common Css</a></li>
+                <li><a href="#Seealso" class="nav-links">See Also</a></li>
             </ul>
         </div>
     </div>
 
-    <div class="col s9">
-        <div style="padding: 1rem;">
-            <h1>Welcome to the Teal Page Content</h1>
-            <p>This is the content area of the page. You can add your content here.</p>
+    <div class="col s10">
+        <div class="">
+            <div class="content-area">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title" style="margin-top: -10px; margin-bottom: -10px;">Dashboard</span>
+                        <!-- <div class="divider"></div> -->
+                        <!-- <h4>Hello</h4> -->
+                    </div>
+                    <div class="card-action">
+                        <div style="margin-top: -10px;">
+                            <table class="highlight responsive-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Item Name</th>
+                                        <th>Item Price</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Alvin</td>
+                                        <td>Eclair</td>
+                                        <td>$0.87</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Alan</td>
+                                        <td>Jellybean</td>
+                                        <td>$3.76</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jonathan</td>
+                                        <td>Lollipop</td>
+                                        <td>$7.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
